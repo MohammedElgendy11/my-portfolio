c24bcf100962d5b53627cb323d5573e8939f1d7b
MohammedElgendy11 <159758548+chromaversecrew@users.noreply.github.com>
2025-08-02 01:18:15 +0300

    Update vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/stellar-game-folio/', // ✅ Add this line for GitHub Pages
+  base: '/my-portfolio/',
+ // ✅ Add this line for GitHub Pages
   server: {
     host: "::",
     port: 8080,
